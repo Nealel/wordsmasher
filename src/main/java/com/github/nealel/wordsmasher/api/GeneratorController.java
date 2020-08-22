c0a0483c8f54a7553e0d7b65b18f65988a7d7f12
nealel <labneale@gmail.com>
2020-08-22 16:51:20 +0100

    load file names from api

--- src/main/java/com/github/nealel/wordsmasher/api/GeneratorController.java
+++ src/main/java/com/github/nealel/wordsmasher/api/GeneratorController.java
@@ -1,11 +1,13 @@
 package com.github.nealel.wordsmasher.api;
 
+import com.github.nealel.wordsmasher.corpus.FileCorpusLoader;
 import com.github.nealel.wordsmasher.generator.BatchGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -13,9 +15,16 @@ public class GeneratorController {
 
     @Autowired
     private BatchGenerator generator;
+    @Autowired
+    private FileCorpusLoader fileCorpusLoader;
 
     @GetMapping("/names")
     public List<String> getNames(@RequestBody BatchRequestDto request) {
         return generator.generateBatch(request);
+    }
+
+    @GetMapping("/corpuses")
+    public List<String> getCorpuses() throws IOException {
+        return fileCorpusLoader.getAvailableFiles();
     }
 }
--- src/main/java/com/github/nealel/wordsmasher/corpus/FileCorpusLoader.java
+++ src/main/java/com/github/nealel/wordsmasher/corpus/FileCorpusLoader.java
@@ -1,12 +1,36 @@
 package com.github.nealel.wordsmasher.corpus;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
+@Component
 @Slf4j
 public class FileCorpusLoader {
+    private final String fileroot;
+    private final ResourcePatternResolver resourceResolver;
+
+    public FileCorpusLoader(@Value("${generator.corpus.fileroot:/data/btn_rich/}") String fileroot,
+                            ResourcePatternResolver resourceResolver) {
+        this.fileroot = fileroot;
+        this.resourceResolver = resourceResolver;
+    }
+
+    public List<String> getAvailableFiles() throws IOException {
+        return Arrays.stream(resourceResolver.getResources("classpath:" + fileroot + "*.txt"))
+                .map(Resource::getFilename)
+                .map(s -> s.substring(0, s.length() - 4))
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
     public static Set<String> loadCorpus(String fileName, int minSize){
         Scanner scanner = new Scanner(FileCorpusLoader.class.getResourceAsStream(fileName), StandardCharsets.UTF_8)
